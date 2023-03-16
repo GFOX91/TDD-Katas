bf9f71221fccab7fcb580ce780a5cc10ec59a5f9
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-16 18:06:34 +0000

    Updated method to allow any number of numbers in the string with updated unit tests

--- TDD Katas/Calculator.cs
+++ TDD Katas/Calculator.cs
@@ -3,26 +3,16 @@ public class Calculator
 {
     /// <summary>
     /// Create a simple String calculator with a method signature: int Add(string numbers)
-    ///     - The method can take up to two numbers, separated by commas, and will return their sum.
+    ///     - The method can any amount of numbers, separated by commas, and will return their sum.
     ///     - For example “” or “1” or “1,2” as inputs.
     ///     - For an empty string it will return 0.
     /// </summary>
     public object Add(string numbers)
     {
-        var splitNumbers = numbers.Split(
-            new char[] {','},
-            StringSplitOptions.RemoveEmptyEntries);
+        var splitNumbers = numbers
+            .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse);
 
-        if (!splitNumbers.Any())
-        {
-            return 0;
-        }
-
-        if (splitNumbers.Length == 1)
-        {
-            return int.Parse(splitNumbers[0]);
-        }
-
-        return int.Parse(splitNumbers[0]) + int.Parse(splitNumbers[1]);
+        return splitNumbers.Sum();
     }
 }
--- UnitTests/CalculatorTests.cs
+++ UnitTests/CalculatorTests.cs
@@ -24,5 +24,21 @@ public class CalculatorTests
         // Assert
         result.Should().Be(expected);
     }
+
+    [Theory]
+    [InlineData("3", 3)]
+    [InlineData("1,2,3", 6)]
+    [InlineData("10,20,30,40,50", 150)]
+    public void Add_AddsUpToAnyNumbers_WhenStringIsValid(string numbers, int expected)
+    {
+        // Arrange
+        var sut = new Calculator();
+
+        // Act
+        var result = sut.Add(numbers);
+
+        // Assert
+        result.Should().Be(expected);
+    }
 }
 
