c6f58c306210fd7e5335211641e7838a1414bc6e
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-16 17:58:33 +0000

    FIrst kata with 1st test

---
+++ TDD Katas/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace TDD_Katas;
+public class Calculator
+{
+    /// <summary>
+    /// Create a simple String calculator with a method signature: int Add(string numbers)
+    ///     - The method can take up to two numbers, separated by commas, and will return their sum.
+    ///     - For example “” or “1” or “1,2” as inputs.
+    ///     - For an empty string it will return 0.
+    /// </summary>
+    public object Add(string numbers)
+    {
+        var splitNumbers = numbers.Split(',');
+
+        if (!splitNumbers.Any())
+        {
+            return 0;
+        }
+
+        if (splitNumbers.Length == 1)
+        {
+            if (splitNumbers[0].Length == 0)
+            {
+                return 0;
+            }
+            return int.Parse(splitNumbers[0]);
+        }
+
+        return int.Parse(splitNumbers[0]) + int.Parse(splitNumbers[1]);
+    }
+}
---
+++ UnitTests/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD_Katas;
+
+namespace UnitTests;
+
+public class CalculatorTests
+{
+    [Theory]
+    [InlineData("", 0)]
+    [InlineData("1", 1)]
+    [InlineData("1,2", 3)]
+    public void Add_AddsUpToTwoNumbers_WhenStringIsValid(string numbers, int expected)
+    {
+        // Arrange
+        var sut = new Calculator();
+
+        // Act
+        var result = sut.Add(numbers);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
+
