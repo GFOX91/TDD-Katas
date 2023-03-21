eb9b1a7fcd9950c897cf7f98259551ba0db260e8
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-21 17:03:21 +0000

    New challenge started fizz buzz first set of tests passing

---
+++ TDD Katas/FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TDD_Katas;
+
+/// <summary>
+/// 1. Write a “fizzBuzz” method that accepts a number as input and returns it as a String
+/// 
+//<see cref = "https://tddmanifesto.com/exercises/" />
+public class FizzBuzz
+{
+    public string print(int number)
+    {
+        return number.ToString();
+    }
+}
---
+++ UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD_Katas;
+
+namespace UnitTests;
+public class FizzBuzzTests
+{
+    [Theory]
+    [InlineData(3, "3")]
+    [InlineData(5, "5")]
+    [InlineData(7, "7")]
+    public void Print_ReturnsNumberAsString(int number, string output)
+    {
+        // Arrange
+        var sut = new FizzBuzz();
+
+        // Act
+        var result = sut.print(number);
+
+        // Assert
+        result.Should().Be(output);
+    }
+}
