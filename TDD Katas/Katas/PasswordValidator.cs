2873fa113cecc2e1b343f31947a4f86b017f93c4
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-21 17:34:19 +0000

    Password validator started with failing tests

---
+++ TDD Katas/Katas/PasswordValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Katas.Katas;
+
+/// <summary>
+/// Create a function that can be used as a validator for the password field of a user registration form. 
+/// The validation function takes a string as an input and returns a validation result. 
+/// The validation result should contain a boolean indicating if the password is valid or not, 
+/// and also a field with the possible validation errors.
+/// 
+/// Requirements
+/// 1. The password must be at least 8 characters long. If it is not met, 
+/// then the following error message should be returned: “Password must be at least 8 characters”
+/// </summary>
+public class PasswordValidator
+{
+
+    public ValidationResult Validate(string password)
+    {
+        return ValidationResult.Success;
+    }
+}
---
+++ UnitTests/PasswordValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using TDD_Katas.Katas;
+
+namespace UnitTests;
+public class PasswordValidatorTests
+{
+    [Theory]
+    [InlineData("pass")]
+    [InlineData("passwo")]
+    public void Validate_ReturnsInvalid_WhenPasswordLessThen8Characters(string password)
+    {
+        // arrange
+        var sut = new PasswordValidator();
+
+        var validationResult = new ValidationResult("Password must be at least 8 characters");
+
+        // act
+        var result = sut.Validate(password);
+
+        // assert 
+        result.Should().Be(validationResult);
+    }
+}
