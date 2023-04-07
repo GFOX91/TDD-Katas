4fd9c1f7d57975b5eab80f582f87007ba658dc86
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-04-07 09:58:56 +0100

    First failing test for city search Kata

---
+++ TDD Katas/Katas/CitySearch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace TDD_Katas.Katas;
+
+/// <summary>
+/// Implement a city search functionality. The function takes a string (search text) as input and returns the found cities which corresponds to the search text.
+/// 
+/// --Prerequisites
+/// Create a collection of strings that will act as a database for the city names.
+/// City names: Paris, Budapest, Skopje, Rotterdam, Valencia, Vancouver, Amsterdam, Vienna, Sydney, New York City, London, Bangkok, Hong Kong, Dubai, Rome, Istanbul
+/// 
+/// --Requirements
+/// 
+/// 1. If the search text is fewer than 2 characters, then should return no results. (It is an optimization feature of the search functionality.)
+/// 
+/// </summary>
+///<see cref = "https://tddmanifesto.com/exercises/" />
+public class CitySearch
+{
+    private List<string> _cityList = new List<string>
+    {
+        "Paris", 
+        "Budapest", 
+        "Skopje", 
+        "Rotterdam", 
+        "Valencia", 
+        "Vancouver", 
+        "Amsterdam", 
+        "Vienna", 
+        "Sydney", 
+        "New York City", 
+        "London", 
+        "Bangkok", 
+        "Hong Kong", 
+        "Dubai", 
+        "Rome", 
+        "Istanbul"
+    };
+   
+    public List<string> FindMatchingCities(string searchText)
+    {
+        return _cityList;
+    }
+}
